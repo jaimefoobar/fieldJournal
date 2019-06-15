940cf6b95fecf55154405e043b7b306a7faf17d4
Jimmy Emery <jemery@allcampus.com>
2019-06-15 02:58:49 -0500

    adding fr

---
+++ fieldReports/stats.js
@@ -0,0 +1,20 @@
+const stats = [
+  {
+    approaches: 2,
+    date: "6/14/2019",
+    followUpDates: 0,
+    performanceRating: 1,
+    phoneNumbers: 1,
+    weekday: "Thursday",
+  },
+  {
+    approaches: 16,
+    date: "6/15/2019",
+    followUpDates: null,
+    performanceRating: 3,
+    phoneNumbers: 3,
+    weekday: "Friday",
+  },
+];
+
+export default stats;
