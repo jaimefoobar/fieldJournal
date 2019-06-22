3b69a194c17c2de8f5600f3b3280e5cc6b17e041
Jimmy Emery <jemery@allcampus.com>
2019-06-22 02:26:42 -0500

    fixing dates

--- fieldReports/stats.js
+++ fieldReports/stats.js
@@ -1,26 +1,29 @@
 const stats = [
   {
     approaches: 2,
-    date: "6/14/2019",
+    date: "6/13/2019",
     followUpDates: 0,
     performanceRating: 1,
     phoneNumbers: 1,
+    sameDayLays: 0,
     weekday: "Thursday",
   },
   {
     approaches: 16,
-    date: "6/15/2019",
+    date: "6/14/2019",
     followUpDates: 0,
     performanceRating: 3,
     phoneNumbers: 3,
+    sameDayLays: 0,
     weekday: "Friday",
   },
   {
     approaches: 6,
-    date: "6/16/2019",
+    date: "6/15/2019",
     followUpDates: 0,
     performanceRating: 2,
     phoneNumbers: 1,
+    sameDayLays: 0,
     weekday: "Saturday",
   },
   {
@@ -29,7 +32,16 @@ const stats = [
     followUpDates: 0,
     performanceRating: 1,
     phoneNumbers: 1,
+    sameDayLays: 0,
     weekday: "Wednesday",
+  },
+  {
+    approaches: 12,
+    date: "6/21/2019",
+    followUpDates: null,
+    performanceRating: 2,
+    phoneNumbers: 1,
+    weekday: "Friday",
   },
 ];
 
